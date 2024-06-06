98994acf61a8f37c6169d27769fe454212e05c05
Aashish Neupane <165367795+aashish-neu@users.noreply.github.com>
2024-06-06 16:09:56 +0545

    Update Readme file
    Updated readme file with few general project goals

--- script.js
+++ script.js
@@ -3,16 +3,11 @@ const gridButton = document.querySelector("#change-grid-button");
 const mainBody = document.querySelector("body");
 const mainContainer = document.querySelector(".main-container");
 let currentCellOpacity = 60;
-// const cellWidth = 5;
 
 // to create 16*16 grid of square divs
 
 gridButton.addEventListener("click", changeGrid);
 
-
-
-// list of functions
-// make grid
 function makeGrid(num) {
 
     gridButton.textContent = "Change Grid";
@@ -47,15 +42,7 @@ function makeGrid(num) {
             currentDivision.addEventListener("mouseout", () => {
                 // currentDivision.style.backgroundColor = getRandomColors(); working
                 const newRandomColor = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`;
-
-                // let newOpacity = increaseOpacity();
-
                 currentDivision.style.backgroundColor = `${newRandomColor}`;
-                // console.log(increasedOpacity);
-                console.log(newRandomColor);
-
-                // currentDivision.style.opacity = `${currentCellOpacity + 100}%`
-
             })
         }
     }
@@ -81,12 +68,6 @@ function getRandomColors () {
     // const randomColor = `#${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}`;
     console.log(randomColor);
     return randomColor;
-}
-
-function increaseOpacity() {
-    let increasedOpacity = currentCellOpacity + 10;
-    console.log(`Increased Opacity ${increasedOpacity}`);
-    return increasedOpacity;
 }
 
 
