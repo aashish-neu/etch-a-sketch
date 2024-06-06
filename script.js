28f42601afd9fa100b6841f9f994552ee41f407a
Aashish Neupane <165367795+aashish-neu@users.noreply.github.com>
2024-06-06 10:22:44 +0545

    Add hover effect
    Added hover effects to each square

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
+// global variables declaration
+const breakLine = document.createElement("br");
+
 // to create 16*16 grid of square divs
 const mainContainer = document.querySelector(".main-container");
 
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
+        
         let currentDivision = document.createElement("div");
         currentDivision.classList.add = "each-division";
-        currentDivision.style.backgroundColor = "red";
-        currentDivision.style.borderColor = "blue";
-        currentDivision.textContent = `${i} * ${j}`;
-        currentDivision.style.minWidth = "50px";
-        currentDivision.style.minHeight = "50px";
+        currentDivision.style.border = "1px solid black";
+        // currentDivision.style.backgroundColor = "red";
+        // currentDivision.textContent = `${i}`;
+        currentDivision.style.minWidth = "30px";
+        currentDivision.style.minHeight = "30px";
         mainContainer.appendChild(currentDivision);
+        // to create hover effect
+        currentDivision.addEventListener("mouseover", () => {
+            currentDivision.style.backgroundColor = "black";
+        });
+        currentDivision.addEventListener("mouseout", () => {
+            currentDivision.style.backgroundColor = "blue";
+        })
     }
 }
+
+function changeColor () {
+    currentDivision.style.backgroundColor = "black";
+}
+
