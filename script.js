a8e35a0a70b3040092be5aa73cb86763bba467e8
Aashish Neupane <165367795+aashish-neu@users.noreply.github.com>
2024-06-06 13:45:59 +0545

    Make single prompt for grid creation
    Removed rows and column prompt and added grid
    prompt

--- script.js
+++ script.js
@@ -1,31 +1,74 @@
 // global variables declaration
 const breakLine = document.createElement("br");
-
-// to create 16*16 grid of square divs
+const changeGridButton = document.querySelector("#change-grid-button");
+const mainBody = document.querySelector("body");
 const mainContainer = document.querySelector(".main-container");
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        
-        let currentDivision = document.createElement("div");
-        currentDivision.classList.add = "each-division";
-        currentDivision.style.border = "1px solid black";
-        // currentDivision.style.backgroundColor = "red";
-        // currentDivision.textContent = `${i}`;
-        currentDivision.style.minWidth = "30px";
-        currentDivision.style.minHeight = "30px";
-        mainContainer.appendChild(currentDivision);
-        // to create hover effect
-        currentDivision.addEventListener("mouseover", () => {
-            currentDivision.style.backgroundColor = "black";
-        });
-        currentDivision.addEventListener("mouseout", () => {
-            currentDivision.style.backgroundColor = "blue";
-        })
+// to create 16*16 grid of square divs
+
+changeGridButton.addEventListener("click", changeGrid);
+
+
+
+// list of functions
+// make grid
+function makeGrid(num) {
+
+    changeGridButton.textContent = "Change Grid";
+
+    deleteGrid();
+
+    // to clear previously created grid
+
+    let gridNumber = num;
+
+    // to find out max width
+    const maximumWidth = gridNumber * 30 + 30;
+    console.log(maximumWidth);
+    console.log(typeof(maximumWidth));
+
+    // const mainContainer = document.createElement("div");
+    // mainContainer.classList.add = "main-container";
+    mainContainer.style.maxWidth = `${maximumWidth}px`;
+    // mainContainer.style.backgroundColor = "blue";
+    // mainContainer.style.display = "flex";
+    // mainContainer.style.flexWrap = "wrap";
+    // mainBody.appendChild(mainContainer);
+
+    // to remove previously created grids inside main container
+
+    for (let i = 0; i < gridNumber; i++) {
+        for (let j = 0; j < gridNumber; j++) {
+           
+            let currentDivision = document.createElement("div");
+            currentDivision.classList.add = "each-division";
+            currentDivision.style.border = "1px solid orange";
+            //main container width
+
+            // mainContainer.style.maxWidth = "maxWidth";
+
+
+
+            currentDivision.style.minWidth = "30px";
+            currentDivision.style.minHeight = "30px";
+            mainContainer.appendChild(currentDivision);
+            // to create hover effect
+            currentDivision.addEventListener("mouseover", () => {
+                currentDivision.style.backgroundColor = "green";
+            });
+            currentDivision.addEventListener("mouseout", () => {
+                currentDivision.style.backgroundColor = "red";
+            })
+        }
     }
 }
 
-function changeColor () {
-    currentDivision.style.backgroundColor = "black";
+function deleteGrid() {
+    mainContainer.textContent = "";
 }
 
+function changeGrid() {
+
+    gridNumber = Number(prompt("How many rows?"));
+    makeGrid(gridNumber);
+}
